9b64c060143f98de244f3adb49d8658fd923c8e8
DrewRL1111 <112494318+DrewRL1111@users.noreply.github.com>
2024-11-18 16:04:38 -0500

    Create budget_view_model.dart

---
+++ budget_buddy_proj/lib/view_model/budget_view_model.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class BudgetViewModel extends ChangeNotifier {
+  double plannedBudget = 0.0;
+  double totalSpending = 0.0;
+  Map<String, double> categorySpending = {};
+
+  void updatePlannedBudget(double newBudget) {
+    plannedBudget = newBudget;
+    notifyListeners();
+  }
+
+  void addSpending(String category, double amount) {
+    if (categorySpending.containsKey(category)) {
+      categorySpending[category] = categorySpending[category]! + amount;
+    } else {
+      categorySpending[category] = amount;
+    }
+    totalSpending += amount;
+    notifyListeners();
+  }
+
+  void resetBudget() {
+    plannedBudget = 0.0;
+    totalSpending = 0.0;
+    categorySpending.clear();
+    notifyListeners();
+  }
+
+  double get remainingBalance {
+    return plannedBudget - totalSpending;
+  }
+
+  List<MapEntry<String, double>> get sortedCategorySpending {
+    var sorted = categorySpending.entries.toList()
+      ..sort((a, b) => b.value.compareTo(a.value)); // Sort descending by amount
+    return sorted;
+  }
+}
